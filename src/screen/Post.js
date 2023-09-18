c6fe1c5d7611e6f66224a0755fcceab2dd1dada8
Long Hai <127493031+long110902kk@users.noreply.github.com>
2023-09-18 15:14:24 +0700

    update search

--- src/screen/Post.js
+++ src/screen/Post.js
@@ -1,37 +1,33 @@
-import { useEffect, useState } from "react";
-import { Col, Container, Row, Table } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row, Table, Button } from "react-bootstrap";
 import DefaultTemplate from "../template/DefaultTemplate";
 import axios from 'axios';
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { PencilSquare, Trash3Fill } from 'react-bootstrap-icons';
+import ReactPaginate from 'react-paginate';
 
 const Post = () => {
     const [students, setStudents] = useState([]);
-    const navigate = useNavigate();
+    const [currentPage, setCurrentPage] = useState(0);
+    const [searchText, setSearchText] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const itemsPerPage = 10; // Số lượng mục trên mỗi trang
 
-    //Call API la user
+    // Call API lấy danh sách sinh viên
     useEffect(() => {
         axios.get('http://localhost:9999/students')
             .then(function (response) {
-                setStudents(response.data)
-                console.log(response.data);
+                setStudents(response.data);
             })
             .catch(function (error) {
                 console.log(error);
             })
-    }, [])
+    }, []);
 
-    // useEffect(() => {
-    //     fetch('http://localhost:9999/users')
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             setUser(data);
-    //         })
-    //         .catch(err => {
-    //             console.log(err.message);
-    //         });
-    // }, [])
+    const pageCount = Math.ceil(students.length / itemsPerPage);
 
+    const offset = currentPage * itemsPerPage;
+    const currentPageData = searchResults.length > 0 ? searchResults : students.slice(offset, offset + itemsPerPage);
 
     // Sort
     const sortStu = (type) => {
@@ -40,8 +36,9 @@ const Post = () => {
             newSort.sort((a, b) => b.id - a.id);
         if (type === 'name')
             newSort.sort((a, b) => a.name > b.name ? 1 : -1);
-        setStudents(newSort)
+        setStudents(newSort);
     }
+
     function handeleDelete(id) {
         // Kiểm tra xem sinh viên có tồn tại trước khi thực hiện xóa
         const studentToDelete = students.find(student => student.id === id);
@@ -63,6 +60,13 @@ const Post = () => {
             });
     }
 
+    const handleSearch = () => {
+        const filteredStudents = students.filter((student) =>
+            student.name.toLowerCase().includes(searchText.toLowerCase())
+        );
+
+        setSearchResults(filteredStudents);
+    };
 
     return (
         <DefaultTemplate>
@@ -70,21 +74,37 @@ const Post = () => {
                 <Container className="post-content">
                     <Row>
                         <Col xs={12}>
-                            <h2 style={{ textAlign: 'center' }}>Post Management</h2>
+                            <h2 style={{ textAlign: 'center' }}>List Student</h2>
                         </Col>
                     </Row>
                     <Row>
                         <Col xs={12}>
-                            <div><Link to="/create">Add</Link></div>
-                            <Table>
+                            <div className="mb-3 d-flex align-items-center">
+                                <Link to="/create" className="btn btn-primary mb-2 me-2">Add</Link>
+                                <div className="ml-auto">
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-sm"
+                                        placeholder="Search"
+                                        value={searchText}
+                                        onChange={(e) => setSearchText(e.target.value)}
+                                    />
+                                </div>
+                                <Button
+                                    variant="secondary"
+                                    size="sm"
+                                    className="ml-2"
+                                    onClick={handleSearch}
+                                >
+                                    Search
+                                </Button>
+                            </div>
+
+                            <Table striped bordered hover>
                                 <thead>
                                     <tr>
-                                        <th>
-                                            <a href="#" onClick={() => sortStu('id')}>Id</a>
-                                        </th>
-                                        <th>
-                                            <a href="#" onClick={() => sortStu('name')}>Name</a>
-                                        </th>
+                                        <th onClick={() => sortStu('id')}>Id</th>
+                                        <th onClick={() => sortStu('name')}>Name</th>
                                         <th>Age</th>
                                         <th>Phone</th>
                                         <th>Action</th>
@@ -92,21 +112,29 @@ const Post = () => {
                                 </thead>
                                 <tbody>
                                     {
-                                        students.map(s => (
+                                        currentPageData.map(s => (
                                             <tr key={s.id}>
                                                 <td>{s.id}</td>
                                                 <td>{s.name}</td>
                                                 <td>{s.age}</td>
                                                 <td>{s.phone}</td>
                                                 <td>
-                                                    <Link to={`/update/${s.id}`} ><PencilSquare /></Link>&ensp;
-                                                    <Link onClick={() => handeleDelete(s.id)}><Trash3Fill /></Link>
+                                                    <Link to={`/update/${s.id}`} className="btn btn-warning btn-sm me-2"><PencilSquare /></Link>
+                                                    <Button variant="danger" size="sm" onClick={() => handeleDelete(s.id)}><Trash3Fill /></Button>
                                                 </td>
                                             </tr>
                                         ))
                                     }
                                 </tbody>
                             </Table>
+                            <ReactPaginate
+                                previousLabel={"Previous"}
+                                nextLabel={"Next"}
+                                pageCount={pageCount}
+                                onPageChange={({ selected }) => setCurrentPage(selected)}
+                                containerClassName={"pagination justify-content-center"}
+                                activeClassName={"active"}
+                            />
                         </Col>
                     </Row>
                 </Container>
