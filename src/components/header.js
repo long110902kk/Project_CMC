d5d6d7361d5dbc22482c3d53bc2e4d08bb5414e1
Long Hai <127493031+long110902kk@users.noreply.github.com>
2023-09-25 09:17:10 +0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,34 @@
-import { Container, Row, Nav } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import { Container, Row, Nav, Dropdown } from 'react-bootstrap';
+import { NavLink, useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
 
 const HeaderApp = () => {
+    const usenavigate = useNavigate();
+
+    useEffect(() => {
+        let email = sessionStorage.getItem('email');
+        if (email === '' || email === null) {
+            usenavigate('/');
+        }
+    }, [usenavigate]);
+
     return (
         <Row className="header-app">
             <Container>
                 <Nav className="justify-content-center">
-                    <div className="nav-link-container">
-                        <NavLink to={'/'}>Home</NavLink>
+                    <div className="nav-link-container header-app-menu">
+                        <NavLink to={'/home'}>Profile</NavLink>
                     </div>
-                    <div className="nav-link-container">
-                        <NavLink to={'/post'}>Post</NavLink>
-                    </div>
-                    <div className="nav-link-container">
-                        <NavLink to={'/login'}>Login</NavLink>
-                    </div>
-                    <div className="nav-link-container">
-                        <NavLink to={'/register'}>Register</NavLink>
+                    <Dropdown as={Nav.Item} className="nav-link-container header-app-menu">
+                        <Dropdown.Toggle as={NavLink} to={'/post'}>List Student</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item as={NavLink} to={'/post'} style={{color: 'black', backgroundColor: 'white'}}>List Student</Dropdown.Item>
+                            <Dropdown.Item as={NavLink} to={'/create'} style={{color: 'black'}}>Add Student</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <div className="nav-link-container header-app-menu">
+                        <NavLink to={'/'}>Logout</NavLink>
                     </div>
                 </Nav>
             </Container>
